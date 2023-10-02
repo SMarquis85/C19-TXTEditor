49c342e7c50601895dfa3dcbba48295c5d69fc4d
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-02 19:49:27 -0400

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,9 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+let txtEditorDB; // Declare a variable to store the database connection.
+
+const initdb = async () => {
+  txtEditorDB = await openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -11,21 +13,20 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+};
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const txtEditorDB = await openDB('jate', 1);
   const transVar = txtEditorDB.transaction('jate', 'readwrite');
   const storeVar = transVar.objectStore('jate');
   const request = storeVar.put({ id: 1, value: content });
-  
+
   const result = await request;
   console.log('Data saved to database', result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const txtEditorDB = await openDB('jate', 'readonly');
   const transVar = txtEditorDB.transaction('jate', 'readonly');
   const storeVar = transVar.objectStore('jate');
   const request = storeVar.get(1);
@@ -34,14 +35,7 @@ export const getDb = async () => {
     ? console.log('Data retrieved from the database', result.value)
     : console.log('Data not found in the database');
 
-    /*
-  if(!result) {
-    throw Error;
-  }
-  return result.value
-  */
-  return result?.value
- ;
+  return result?.value;
 };
 
 initdb();
