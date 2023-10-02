ce908edb4f5b32a0bf10e38e2bbbdaba0fbe991a
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-02 17:08:24 -0400

    updated files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const transVar = txtEditorDB.transaction('jate', 'readwrite');
   const storeVar = transVar.objectStore('jate');
   const request = storeVar.put({ id: 1, value: content });
-
+  
   const result = await request;
   console.log('Data saved to database', result.value);
 };
@@ -26,12 +26,22 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const txtEditorDB = await openDB('jate', 'readonly');
-  const storeVar = trasnVar.objectStore('jate');
+  const transVar = txtEditorDB.transaction('jate', 'readonly');
+  const storeVar = transVar.objectStore('jate');
   const request = storeVar.get(1);
   const result = await request;
   result
     ? console.log('Data retrieved from the database', result.value)
     : console.log('Data not found in the database');
+
+    /*
+  if(!result) {
+    throw Error;
+  }
+  return result.value
+  */
+  return result?.value
+ ;
 };
 
 initdb();
