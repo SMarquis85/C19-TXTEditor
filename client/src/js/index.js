4d8d528a5cc02af1ecfb5ff2d0281f1de1b94b09
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-02 20:51:59 -0400

    updated index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,6 +2,7 @@ import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
+import { header } from './path-to-header-file';
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
