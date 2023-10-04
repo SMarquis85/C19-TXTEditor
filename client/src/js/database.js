897871b1c3c2279259b67dccb3751601ea793012
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-04 12:49:14 -0400

    updated database.js index.js index.html src-sw.js file and updated packages

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,28 +15,55 @@ const initdb = async () => {
   });
 };
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log(txtEditorDB);
-  const transVar = txtEditorDB.transaction('jate', 'readwrite');
-  const storeVar = transVar.objectStore('jate');
-  const request = storeVar.put({ id: 1, value: content });
+  try {
+    if (!txtEditorDB) {
+      throw new Error('Database is not initialized.');
+    }
 
-  const result = await request;
-  console.log('Data saved to database', result.value);
+    const transVar = txtEditorDB.transaction('jate', 'readwrite');
+    const storeVar = transVar.objectStore('jate');
+    const request = storeVar.put({ value: content });
+
+    const result = await request;
+    console.log('Data saved to database with key:', result);
+  } catch (error) {
+    console.error('Error while putting data into the database:', error);
+  }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const transVar = txtEditorDB.transaction('jate', 'readonly');
-  const storeVar = transVar.objectStore('jate');
-  const request = storeVar.get(1);
-  const result = await request;
-  result
-    ? console.log('Data retrieved from the database', result.value)
-    : console.log('Data not found in the database');
+  try {
+    if (!txtEditorDB) {
+      throw new Error('Database is not initialized.');
+    }
 
-  return result?.value;
+    const transVar = txtEditorDB.transaction('jate', 'readonly');
+    const storeVar = transVar.objectStore('jate');
+    const request = storeVar.get(1);
+    const result = await request;
+    if (result) {
+      console.log('Data retrieved from the database', result.value);
+    } else {
+      console.log('Data not found in the database');
+    }
+    return result?.value;
+  } catch (error) {
+    console.error('Error while getting data from the database:', error);
+    return null; // Return null or handle the error accordingly
+  }
 };
 
-initdb();
+// Function to start the application and initialize the database
+export const startApp = async () => {
+  await initdb();
+
+  // Now you can safely use txtEditorDB
+  // Your other application logic here
+};
+
+// Example of how to use the code
+startApp(); // Initialize the database
+// Your other application logic here, including using getDb and putDb
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,78 +1,34 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+import { Workbox } from 'workbox-window';
+import Editor from './editor';
+import './database';
+import '../css/style.css';
+import { header } from './header';
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/js/index.js',
-    install: './src/js/install.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    // Generate HTML files based on templates
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      title: 'JATE',
-    }),
+const main = document.querySelector('#main');
+main.innerHTML = '';
 
-    // Extract CSS into separate files
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-
-    // Generate service worker using Workbox
-    new InjectManifest({
-      swSrc: './src-sw.js',
-      swDest: 'src-sw.js',
-    }),
-
-    // Generate a web app manifest file
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: true,
-      name: 'Just Another Text Editor',
-      short_name: 'J.A.T.E.',
-      description: "This application installs 'Just Another Text Editor' or 'JATE' for short.",
-      background_color: '#225ca3',
-      theme_color: '#225ca3',
-      start_url: '/',
-      publicPath: '/',
-      icons: [
-        {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        },
-      ],
-    }),
-  ],
-
-  module: {
-    rules: [
-      // Process CSS files and extract them using MiniCssExtractPlugin
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      // Transpile JavaScript using Babel
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-          },
-        },
-      },
-    ],
-  },
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
 };
+
+const editor = new Editor();
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
+
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
+  workboxSW.register();
+} else {
+  console.error('Service workers are not supported in this browser.');
+} 
